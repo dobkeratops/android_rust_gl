8886333731cb0b6203a8c4b3ba169dfb19237698
dobkeratops <dobkeratopsgit@gmail.com>
2014-07-14 01:33:51 +0100

    ok

---
+++ bsprender.rs
@@ -0,0 +1,45 @@
+use r3d::*;
+use bsp::*;
+
+// renderable version of a bsp..
+pub struct BspRender {
+	bsp:Box<Blob<BspHeader>>,
+	textures:Vec<GLuint>,
+	// vertex/index arrays would go here
+}
+impl BspRender {
+	pub fn new(bsp:Box<Blob<BspHeader>>)->BspRender {
+		let mut tex_array=Vec::new();
+
+		bsp.visit_textures( &mut |i,_|{
+				let (tx,img)=bsp.get_texture_image(i); 
+				let txsize=(tx.width as u32,tx.height as u32);
+				let txi=create_texture((tx.width,tx.height), &img,8);
+				tex_array.push(txi);
+			}
+		);
+
+
+		BspRender{
+			bsp:bsp,
+			textures:tex_array,
+		}
+	}
+
+	pub fn render(&self) {
+		self.bsp.visit_triangles(
+			&mut |_,(v0,v1,v2),(_,txinfo),(_,plane),(face_id,_)| {
+				unsafe {
+					let txi=txinfo.miptex as uint;
+					draw_set_texture(0,*self.textures.get(txi));
+			
+					fn applytx<'a>(tx:&'a TexInfo,v:&'a BspVec3)->(&'a BspVec3,(f32,f32)){
+						(v, (v3dot(&tx.axis_s,v)+tx.ofs_s,v3dot(&tx.axis_t,v)+tx.ofs_t) )
+					}
+					let scale=1.0f32/2000.0f32;
+					draw_tri_tex(applytx(txinfo,v0),applytx(txinfo,v1),applytx(txinfo,v2), 0xffffff,scale)
+				}
+			}
+		);
+	}
+}
